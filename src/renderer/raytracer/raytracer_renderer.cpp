cc7306c12c280175148bdc58bea35b98e1d98e62
Sergey Milgram <Milgram_S@mail.ru>
2022-06-10 13:39:20 +0300

    Lab 2.04. Adjust closest_hit_shader of raytracer to cast shadows rays and to ignore occluded lights

--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -39,6 +39,8 @@ void cg::renderer::ray_tracing_renderer::init()
 	lights.push_back({float3{0.f, 1.58f, -0.03f}, float3{0.78f, 0.78f, 0.78f}});
 
 	shadow_raytracer = std::make_shared<cg::renderer::raytracer<cg::vertex, cg::unsigned_color>>();
+	shadow_raytracer->set_vertex_buffers(model->get_vertex_buffers());
+	shadow_raytracer->set_index_buffers(model->get_index_buffers());
 }
 
 void cg::renderer::ray_tracing_renderer::destroy() {}
@@ -47,6 +49,18 @@ void cg::renderer::ray_tracing_renderer::update() {}
 
 void cg::renderer::ray_tracing_renderer::render()
 {
+	shadow_raytracer->miss_shader = [](const ray& ray)
+	{
+		payload payload{};
+		payload.t = -1.f;
+		return payload;
+	};
+
+	shadow_raytracer->any_hit_shader = [](const ray& ray, payload& payload, const triangle<cg::vertex>& triangle)
+	{
+		return payload;
+	};
+
 	raytracer->clear_render_target({0, 0, 0});
 	raytracer->miss_shader = [](const ray& ray)
 	{
@@ -63,25 +77,18 @@ void cg::renderer::ray_tracing_renderer::render()
 		float3 result_color = triangle.emissive;
 		for (auto& light : lights) {
 			cg::renderer::ray to_light(position, light.position - position);
-			result_color += triangle.diffuse * light.color * std::max(dot(normal, to_light.direction), 0.f);
+			auto shadow_payload = shadow_raytracer->trace_ray(to_light, 1, length(light.position - position));
+			if (shadow_payload.t < 0.f)
+			{
+				result_color += triangle.diffuse * light.color * std::max(dot(normal, to_light.direction), 0.f);
+			}
 		}
 
 		payload.color = cg::color::from_float3(result_color);
 		return payload;
 	};
 	raytracer->build_acceleration_structure();
-
-	shadow_raytracer->miss_shader = [](const ray& ray)
-	{
-		payload payload{};
-		payload.t = -1.f;
-		return payload;
-	};
-
-	shadow_raytracer->any_hit_shader = [](const ray& ray, payload& payload, const triangle<cg::vertex>& triangle)
-	{
-		return payload;
-	};
+	shadow_raytracer->build_acceleration_structure();
 
 	auto start = std::chrono::high_resolution_clock::now();
 	raytracer->ray_generation(
@@ -96,6 +103,5 @@ void cg::renderer::ray_tracing_renderer::render()
 	std::chrono::duration<float, std::milli> raytracing_duration = stop - start;
 	std::cout << "Raytracing took " << raytracing_duration.count() << "ms\n";
 	cg::utils::save_resource(*render_target, settings->result_path);
-	// TODO: Lab 2.04. Adjust closest_hit_shader of raytracer to cast shadows rays and to ignore occluded lights
 	// TODO: Lab 2.05. Adjust ray_tracing_renderer class to build the acceleration structure
 }
